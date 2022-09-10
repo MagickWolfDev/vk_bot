20dfaebf4d969e712850ccc8fbc2d5edb504c49b
wolf <ukhabatov@mail.ru>
2022-09-10 11:53:54 +0300

    second fix

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const VkBot = require('node-vk-bot-api/lib');
 const Markup = require('node-vk-bot-api/lib/markup');
+const Comands = require('./comands')
 
+require('dotenv').config()
+
+const bot = new VkBot(process.env.TOKEN);
+const comands = new Comands(bot);
+
+comands.comandHandler();
+
+bot.startPolling();
+
+console.log('startup sucsessful!');
